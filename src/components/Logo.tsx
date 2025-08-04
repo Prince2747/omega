ab7f9d6979e7ef01705e6163a9159e2a26fce307
Leul Yewondwossen <leulboy123@gmail.com>
2025-08-04 17:27:37 +0000

    edit the svg remove most of the white space

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
     <svg
       className={className}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 813 813"
+      viewBox="0 2000 8130 4000"
       preserveAspectRatio="xMidYMid meet"
       fill="currentColor"
     >
